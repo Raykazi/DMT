e21383c79c11b7b61e7bdf192656685bcc55125b
Raykazi <reykazi@gmail.com>
2020-09-01 21:29:34 -0400

    JBs auto sync implemented. Fixed JB Sync as well.

---
+++ Models/DMTBridges.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.Models
+{
+    public class DMTBridges
+    {
+        public List<string> Bridges { get; set; }
+        public DateTimeOffset LastModified { get; set; }
+    }
+}
